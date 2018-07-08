89a336adbf5fa28461b515792ceba888eca64426
Roy <royachmad06@gmail.com>
2018-07-09 01:26:47 +0700

    model cetak

---
+++ application/controllers/Cetak.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cetak extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('Cetak_model');
+		$this->load->library('dompdf_gen');
+		$this->load->helper('url','file');
+	}
+
+	public function index($id){
+		$data['buku']=$this->Cetak_model->getIdBuku($id);
+	
+		$this->load->view('preview_buku', $data);
+	}
+
+	public function cetakBuku($id){
+		$data['buku']=$this->Cetak_model->getIdBuku($id);
+		$this->load->view('print_buku', $data);
+		$paper_size = 'A4';
+		$orientation = 'landscape';
+		$html = $this->output->get_output();
+
+		$dompdf = new DOMPDF();
+		$this->dompdf->load_html($html);
+		$this->dompdf->render();
+		$this->dompdf->stream("Sinopsis_Buku.pdf");
+		unset($html);
+		unset($dompdf);
+	}
+
+	// public function cetakExcel($id)
+ // 	{
+ // 		$data = array( 'title' => 'Buku Excel',
+	// 	'buku' => $this->Cetak_model->getIdBuku($id));
+	// 	$this->load->view('print_excel',$data);
+ // 	}
+
+	// public function cetakWord()
+ // 	{
+ // 		$data = array( 'title' => 'Buku Word',
+	// 	'buku' => $this->Cetak_model->view());
+	// 	$this->load->view('print_word',$data);
+ // 	}
+
+
+}
+
+/* End of file Cetak.php */
+/* Location: ./application/controllers/Cetak.php */
---
+++ application/models/Cetak_model.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Cetak_model extends CI_Model {
+
+	public function view(){
+		$this->db->select("kd_buku, judul, penulis, img, penerbit, genre, sinopsis");
+		$query = $this->db->get('buku');
+		return $query->result();
+	}
+
+	public function getIdBuku($id){
+		$this->db->where('kd_buku',$id);
+		$query = $this->db->get('buku');
+		return $query->result();
+	}
+	
+	
+
+}
+/* End of file cetak_model.php */
+/* Location: ./application/models/cetak_model.php */
